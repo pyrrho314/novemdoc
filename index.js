495ab10332d3051b3679b6ca2c118ee1e07ddc61
callen <craig@novem.technology>
2022-11-13 16:15:19 -0500

    merging new fe branch

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import NovemDoc from './novemdoc.js';
 import ndocConfig, {loadConfig} from './config.js';
 import DogLogger from './doglogger/doglogger.js';
+import NDocRecipe from './ndoc_recipes/NDocRecipe.js';
 
 import {prettyJson, shortJson} from './misc/pretty.js'
+
+const RecipeEngine = NDocRecipe;
 
 export {
     NovemDoc, DogLogger,
     ndocConfig, loadConfig,
     prettyJson, shortJson,
+    RecipeEngine,
 };
